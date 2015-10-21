782e53768eb1328d85f6496466b83798c01a7cbd
danfergo@gmai.com <danfergo102>
2015-10-21 23:40:35 +0100

    basic platform structure.

---
+++ client/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name amepApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the amepApp
+ */
+angular.module('amepApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
