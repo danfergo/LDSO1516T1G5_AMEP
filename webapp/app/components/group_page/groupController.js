39646fa31e56c08edbc65c66ca74d64b633c86f2
danfergo <danfergo@gmail.com>
2015-11-23 14:27:32 +0000

    some group updates

--- webapp/app/components/group_page/groupController.js
+++ webapp/app/components/group_page/groupController.js
@@ -1,21 +1,35 @@
 angular.module('amep-group-page').
-controller('groupController', ['$scope', '$state', 'currentGroup', '$mdDialog', function ($scope, $state, currentGroup, $mdDialog) {
+controller('groupController', ['$scope', '$state', 'currentGroup', '$mdDialog', 'Group', 'currentSession', function ($scope, $state, currentGroup, $mdDialog, Group, currentSession) {
   if (!currentGroup.id) $state.go('404');
 
   $scope.group = currentGroup;
+  $scope.currentSession = currentSession;
 
   $scope.joinGroup = function (ev) {
-    console.log('xx');
+    var message = 'Se ainda não o fizeste sugerimos-te que entres em contacto com os <br>responsáveis do grupo e te apresentes.';
+
+
     var confirm = $mdDialog.confirm()
       .title('Estás prestes a aderir ao grupo "' + $scope.group.name + '"')
-      .content('Se ainda não o fizeste sugerimos-te que entres em contacto com os <br>responsáveis do grupo e te apresentes.')
-      .ariaLabel('Lucky day')
+      .content(message)
+      .ariaLabel('Novo grupo')
       .targetEvent(ev)
       .cancel('Cancelar')
       .ok('Confirmar');
 
     $mdDialog.show(confirm).then(function () {
 
+      if (currentSession.id) {
+        Group.save(group, function (group) {
+          $mdToast.show(
+            $mdToast.simple()
+              .content('Novo grupo criado com o nome ' + group.name)
+              .hideDelay(1000)
+          );
+        })
+      } else {
+        $state.go('signUp', {join: currentGroup.id});
+      }
     });
   }
 }]);
