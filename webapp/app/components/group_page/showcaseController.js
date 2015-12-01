4d094b60739210c8a4dd76ee7f1fd9f1a8586364
danfergo <danfergo@gmail.com>
2015-12-01 15:13:30 +0000

    popup added. products cycle management

--- webapp/app/components/group_page/showcaseController.js
+++ webapp/app/components/group_page/showcaseController.js
@@ -1,4 +1,18 @@
 angular.module('amep-group-page').
-controller('groupShowcaseController', ['$scope', function ($scope) {
-
+controller('groupShowcaseController', ['$scope','$mdDialog', function ($scope,$mdDialog) {
+   console.log(angular.element(document.body));
+    $scope.openCycleProductsManagement = function(ev){
+      $mdDialog.show({
+        controller: 'manageCycleProductsController',
+        templateUrl: 'components/group_page/manageCycleProducts.html',
+        targetEvent: ev,
+        clickOutsideToClose:false,
+      })
+      .then(function(answer) {
+        $scope.status = 'You said the information was "' + answer + '".';
+      }, function() {
+        $scope.status = 'You cancelled the dialog.';
+      });
+    }
+  $scope.openCycleProductsManagement();
 }]);
