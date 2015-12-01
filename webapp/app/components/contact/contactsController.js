00f31b79462ac93059d2b5b4f2bbe4f4714d723e
filipaivars <ivars.filipa@gmail.com>
2015-12-01 02:06:28 +0000

    contact form

--- webapp/app/components/contact/contactsController.js
+++ webapp/app/components/contact/contactsController.js
@@ -1,11 +1,27 @@
 angular.module('amep-contacts').
-controller('contactController', ['$scope',function ($scope) {
-  //$scope.showConfirmationMessage = false;
-  //$scope.email = null;
+controller('contactController', ['$scope', 'currentSession', '$http', '$mdToast',function ($scope, currentSession, $http, $mdToast) {
+  $scope.currentSession = currentSession;
+  $scope.error = null;
 
-  $scope.contactSend = function (assunto, myMessage) {
-    $scope.assunto = angular.copy(email);
-    $scope.myMessage = angular.copy(myMessage);
-    $scope.errorMessages = null;
+  $scope.contactSend = function (nome, email, tlm, assunto, myMessage) {
+    $scope.error = null;
+    $scope.email = angular.copy(email);
+    console.log($scope.nome);
+    console.log($scope.email);
+    $http.post('/api/v1/contact-form', {
+      nome: nome,
+      email: email,
+      tlm: tlm,
+      assunto: assunto,
+      myMessage: myMessage
+    }).then(function(){
+      $mdToast.show(
+        $mdToast.simple()
+          .content('A sua mensagem foi enviada!')
+          .hideDelay(1000)
+      );
+    }, function(){
+      $scope.error = "Erro: Tente mais tarde!";
+    });
   }
 }]);
