16396c6a2e4f15e13876593b282bbf48de7fa75c
filipaivars <ivars.filipa@gmail.com>
2015-12-01 02:06:28 +0000

    contact form

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -11,13 +11,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151118220104) do
+ActiveRecord::Schema.define(version: 20151130185826) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "cities", force: :cascade do |t|
     t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "contact_forms", force: :cascade do |t|
+    t.string   "assunto"
+    t.string   "myMessage"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "message"
+    t.string   "subject"
+    t.string   "cell"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
