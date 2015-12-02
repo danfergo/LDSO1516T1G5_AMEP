10c3a5a824645e650020131d44aa75b5cc851da4
kdfky <1+4+3>
2015-12-02 19:27:57 +0000

    Settings and agenda submitted

--- webapp/app/components/p_dashboard/agenda/agendaController.js
+++ webapp/app/components/p_dashboard/agenda/agendaController.js
@@ -1,4 +1,8 @@
 angular.module('amep-prossumer-dashboard').
-controller('agendaController',['$scope', 'Session', function ($scope,Session) {
+controller('agendaController',['$scope', 'Agenda', function ($scope, Agenda) {
+
+  $scope.weeks = Agenda.query();
+
+  console.log($scope.weeks.id);
 
 }]);
