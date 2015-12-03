65e2ead2c38586466bcb225537e2af8071da9073
filipaivars <ivars.filipa@gmail.com>
2015-12-03 01:09:11 +0000

    date cycle

--- webapp/app/components/group_page/historyController.js
+++ webapp/app/components/group_page/historyController.js
@@ -1,4 +1,15 @@
 angular.module('amep-group-page').
 controller('groupHistoryController', ['$scope', 'Cycle', function ($scope, Cycle) {
     $scope.cycles=Cycle.query();
+    $scope.cycleSelected = {};
+    
+
+
+    $scope.startDate = moment().subtract(2, 'months').format('YYYY-MM-DD');
+    $scope.endDate = moment().add(2, 'months').format('YYYY-MM-DD');
+    $scope.events = [{'date': moment().subtract(7, 'days').format('YYYY-MM-DD'), 'content':'<p>lorem ipsum</p>'},
+                     {'date': moment().add(7, 'days').format('YYYY-MM-DD'), 'content':'<p>lorem ipsum</p>'}];
+
+
+
 }]);
