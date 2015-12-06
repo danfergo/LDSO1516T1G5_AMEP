2dbc473a37de6c7d1eedcc9e6bcd60c10ea2ba97
filipaivars <ivars.filipa@gmail.com>
2015-12-06 20:17:34 +0000

    changes history

--- webapp/app/components/group_page/historyController.js
+++ webapp/app/components/group_page/historyController.js
@@ -1,14 +1,22 @@
 angular.module('amep-group-page').
 controller('groupHistoryController', ['$scope', 'Cycle', 'currentGroup', 'currentCycles', function ($scope, Cycle, currentGroup, currentCycles) {
-    $scope.cycles= currentCycles;
-      $scope.cycleSelected = $scope.cycles[$scope.cycles.length - 1];
+  $scope.cycles= currentCycles;
+  $scope.cycleSelected = $scope.cycles[$scope.cycles.length - 2];
 
+  $scope.startDate = moment($scope.cycleSelected.start_time).format('YYYY-MM-DD');
+  $scope.endDate = moment($scope.cycleSelected.end_time).format('YYYY-MM-DD');
+  $scope.events = [
+    {'date': moment().subtract(7, 'days').format('YYYY-MM-DD'), 'content':'<p>lorem ipsum</p>'},
+    {'date': moment().add(7, 'days').format('YYYY-MM-DD'), 'content':'<p>lorem ipsum</p>'}
+  ];
 
-
-      $scope.startDate = moment($scope.cycleSelected.start_time).format('YYYY-MM-DD');
-      $scope.endDate = moment($scope.cycleSelected.end_time).format('YYYY-MM-DD');
-      $scope.events = [{'date': moment().subtract(7, 'days').format('YYYY-MM-DD'), 'content':'<p>lorem ipsum</p>'},
-                       {'date': moment().add(7, 'days').format('YYYY-MM-DD'), 'content':'<p>lorem ipsum</p>'}];
+  $scope.selectChanged = function(){
+    $("#reloadMe").load(location.href + " #reloadMe");
+    $scope.startDate = moment($scope.cycleSelected.start_time).format('YYYY-MM-DD');
+    $scope.endDate = moment($scope.cycleSelected.end_time).format('YYYY-MM-DD');
+    $scope.events = [{'date': moment().subtract(7, 'days').format('YYYY-MM-DD'), 'content':'<p>lorem ipsum</p>'},
+    {'date': moment().add(7, 'days').format('YYYY-MM-DD'), 'content':'<p>lorem ipsum</p>'}];
+  };
 
 
 }]);
