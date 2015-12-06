49ed8c40067551745932a4e4af92ee04bd0a9b96
danfergo <danfergo@gmail.com>
2015-12-06 21:32:56 +0000

    stuff.

--- webapp/app/components/group_page/addProductToCycle.js
+++ webapp/app/components/group_page/addProductToCycle.js
@@ -1,15 +1,15 @@
 angular.module('amep-group-page').
-controller('addProductToCycleController', ['$filter', '$scope', '$mdDialog', 'weeks', 'data', 'Group', function ($filter, $scope, $mdDialog, weeks, data, Group) {
+controller('addProductToCycleController', ['$filter','$mdToast', '$scope', '$mdDialog', 'weeks', 'data', 'Group', function ($filter,$mdToast, $scope, $mdDialog, weeks, data, Group) {
   $scope.weeks = angular.copy(weeks);
   $scope.currentProduct = data ? data.currentProduct : undefined;
   $scope.selectedStock = [];
   $scope.selectWeeks = [];
-
+  $scope.productCategories = data.productCategories;
 
   $scope.addProductToCycle = function () {
     var selectedWeeks = $filter('filter')($scope.weeks, {selected: true});
-    Group.Cycle.Product.save({groupId: data.groupId, cycleId: data.cycleId, id: data.currentProduct.id},{weeks: selectedWeeks}, function(data){
-
+    Group.Cycle.Product.save({groupId: data.groupId, cycleId: data.cycleId, id: data.currentProduct.id},{weeks: selectedWeeks}, function(product){
+      $mdDialog.hide(product);
     });
   };
 
