7cbc5cdc5c3c423e0cdbc65bdcadae25edd97434
kdfky <1+4+3>
2015-12-09 17:57:46 +0000

    agenda layout updated

--- server/app/controllers/agenda_controller.rb
+++ server/app/controllers/agenda_controller.rb
@@ -2,7 +2,7 @@ class AgendaController < ApplicationController
 
   def index
     @groupsIds = Prossumer.find(params[:prossumer_id]).groups.ids
-    @cyclesIds = Cycle.where(:group_id => @groupsIds).where('end_time >= ?', Time.now)
+    @cyclesIds = Cycle.where(:group_id => @groupsIds).where('end_time >= ?', Time.now).where('start_time <= ?', Time.now)
     @weeks = Week.where(cycle_id: @cyclesIds).where('delivery_date > ?', Time.now).order(delivery_date: :asc)
 
     render json: @weeks.as_json(
