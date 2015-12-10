761412600b7e0cacfc28adbe88efe6d12ed4a1fd
filipaivars <ivars.filipa@gmail.com>
2015-12-10 12:07:11 +0000

    cenas

--- webapp/app/components/group_page/historyController.js
+++ webapp/app/components/group_page/historyController.js
@@ -1,5 +1,5 @@
 angular.module('amep-group-page').
-controller('groupHistoryController', ['$scope', 'Group','Cycle', 'currentGroup', 'currentCycles', function ($scope, Group, Cycle, currentGroup, currentCycles) {
+controller('groupHistoryController', ['$scope', 'Group','Cycle', 'currentGroup', 'currentCycles','ngTableParams', function ($scope, Group, Cycle, currentGroup, currentCycles, ngTableParams) {
 
   $scope.cycles= currentCycles;
   $scope.cycleSelected = $scope.cycles[$scope.cycles.length - 2];
@@ -7,5 +7,9 @@ controller('groupHistoryController', ['$scope', 'Group','Cycle', 'currentGroup',
     $scope.getWeeks = Group.Cycle.Week.query({groupId: $scope.cycleSelected.group_id, cycleId: $scope.cycleSelected.id});
   };
   $scope.mudaSemana();
+
+  var self = this;
+var data = [{name: "Moroni", age: 50}, {name: "Moni", age: 45} /*,*/];
+self.tableParams = new ngTableParams({}, { getData: data});
 
 }]);
