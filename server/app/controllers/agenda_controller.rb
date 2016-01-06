c5909422e99d4ba19dcf4e36b1abfddf5ff0f11c
infus <infus@sapo.pt>
2016-01-05 23:20:25 -0500

    fixed showcase

--- server/app/controllers/agenda_controller.rb
+++ server/app/controllers/agenda_controller.rb
@@ -3,7 +3,7 @@ class AgendaController < ApplicationController
 
   def index
     groupsIds = Prossumer.find(params[:prossumer_id]).groups.ids
-    cycles = Cycle.where(:group_id => groupsIds).where('end_time >= ?', Time.now).where('start_time <= ?', Time.now)
+    cycles = Cycle.where(:group_id => groupsIds).where('end_time >= ?', Time.now).where('start_time <= ?', Time.now + 15.days)
     weeks = Week.where(cycle_id: cycles).where('delivery_date > ?', Time.now).order(delivery_date: :asc)
 
 
